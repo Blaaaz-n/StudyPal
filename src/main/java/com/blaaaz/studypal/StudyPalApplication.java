3a8975031aacc2810a901b65bb5d32386ca17bc1
Ahmed <2292583@crosemont.qc.ca>
2025-09-13 23:19:48 -0400

    Initial commit: Spring Boot backend generated with Spring Initializr

---
+++ src/main/java/com/blaaaz/studypal/StudyPalApplication.java
@@ -0,0 +1,13 @@
+package com.blaaaz.studypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blaaaz/studypal/StudyPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaaaz.studypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
