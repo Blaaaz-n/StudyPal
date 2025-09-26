094494c4e019365c492b1e01b0ef130e4b964752
Ahmed <2292583@crosemont.qc.ca>
2025-09-26 01:01:43 -0400

    LoginRequest initial file

---
+++ src/main/java/com/blaaaz/studypal/auth/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.blaaaz.studypal.auth.dto;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() { return email; }
+    public void setEmail(String username) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+
+}
