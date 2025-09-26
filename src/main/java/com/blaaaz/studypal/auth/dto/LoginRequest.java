e2f65ecd4931ee2418292515e77efb8748c72abf
Ahmed <2292583@crosemont.qc.ca>
2025-09-26 02:12:00 -0400

    Updated for Jwt and passwordEncoder

--- src/main/java/com/blaaaz/studypal/auth/dto/LoginRequest.java
+++ src/main/java/com/blaaaz/studypal/auth/dto/LoginRequest.java
@@ -5,10 +5,8 @@ public class LoginRequest {
     private String password;
 
     public String getEmail() { return email; }
-    public void setEmail(String username) { this.email = email; }
+    public void setEmail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
-
 }
