a6b01e118d08e272986e887d5a8214f920cb4a84
Ahmed <2292583@crosemont.qc.ca>
2025-09-26 01:01:02 -0400

    Plan initial file -- uncomplete

--- src/main/java/com/blaaaz/studypal/plan/web/PlanController.java
+++ src/main/java/com/blaaaz/studypal/plan/web/PlanController.java
@@ -1,4 +1,17 @@
 package com.blaaaz.studypal.plan.web;
 
+import com.blaaaz.studypal.plan.service.PlanService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/plans")
 public class PlanController {
+
+    private final PlanService plan;
+
+    public PlanController(PlanService plan) {
+        this.plan = plan;
+    }
 }
