89a927be59cd89cb0ea8006bba3633e32901d692
Ahmed <2292583@crosemont.qc.ca>
2025-09-26 00:58:32 -0400

    AuthController initial file

---
+++ src/main/java/com/blaaaz/studypal/auth/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.blaaaz.studypal.auth.controller;
+
+import com.blaaaz.studypal.auth.dto.AuthResponse;
+import com.blaaaz.studypal.auth.dto.LoginRequest;
+import com.blaaaz.studypal.auth.dto.RegisterRequest;
+import com.blaaaz.studypal.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // register user
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    // login user
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
