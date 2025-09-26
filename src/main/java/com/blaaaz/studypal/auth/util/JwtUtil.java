d41988d810a5318336a8e6a0970609711349bb96
Ahmed <2292583@crosemont.qc.ca>
2025-09-26 01:02:07 -0400

    Util initial file

---
+++ src/main/java/com/blaaaz/studypal/auth/util/JwtUtil.java
@@ -0,0 +1,60 @@
+package com.blaaaz.studypal.auth.util;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);;
+    private static final long EXPIRATION_TIME = 3600000 ; // 1 hour
+
+    // create token w/ id
+    public String generateToken(String userId, String email) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("email", email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    // check if token valid
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // get userId w/ token
+    public String extractUserId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // get email w/ token
+    public String extractUserEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("email", String.class);
+    }
+}
