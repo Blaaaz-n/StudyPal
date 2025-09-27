09026414fe77a6c24f112701b906cafe87cc62c5
Ahmed <2292583@crosemont.qc.ca>
2025-09-27 00:10:29 -0400

    initial commit

---
+++ src/main/java/com/blaaaz/studypal/event/web/EventController.java
@@ -0,0 +1,20 @@
+package com.blaaaz.studypal.event.web;
+
+
+import com.blaaaz.studypal.event.service.EventService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/events")
+public class EventController {
+
+//    private final EventService event;
+//
+//    public EventController(EventService event) {
+//        this.event = event;
+//    }
+
+
+
+}
